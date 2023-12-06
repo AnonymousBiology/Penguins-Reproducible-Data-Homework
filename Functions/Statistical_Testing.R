f32c0d95adf4bbbc0963aad53c82366e74755dad
AnonymousBiology <150140489+AnonymousBiology@users.noreply.github.com>
2023-12-06 19:06:27 +0000

    Delete Functions/Statistical_Testing.R

--- Functions/Statistical_Testing.R
+++
@@ -1,5 +0,0 @@
-# Linear regression between culmen depth and body mass 
-create_linear_model_culmen_depth_body_mass <- function(data) {
-  model <- lm(body_mass_g ~ culmen_depth_mm, data = data)
-  return(model)
-}
